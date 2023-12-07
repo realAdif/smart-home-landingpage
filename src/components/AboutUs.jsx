b4e7de228d6fd49a65a97395b91e0c96fe892987
Aditya <90451018+realAdif@users.noreply.github.com>
2023-12-07 18:17:27 +1100

    Clients page

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,19 +1,28 @@
 import SmallAboutUsImage from '../assets/SmallAboutUs.png';
+import SmallAboutUs2Image from '../assets/SmallAboutUs2.png';
+import SmallAboutUs3Image from '../assets/SmallAboutUs3.png';
+
+import LagerAboutUsImage from '../assets/LargerAboutUs.png';
+import LagerAboutUs2Image from '../assets/LargerAboutUs2.png';
+import LagerAboutUs3Image from '../assets/LargerAboutUs3.png';
 
 const aboutUsCards = [
   {
+    lagerImage: LagerAboutUsImage,
     image: SmallAboutUsImage,
     title: "Smart Home's Smart Services",
     p: 'Ye am depending propriety sweetness distrusts belonging collected. Smiling mention he in thought equally musical. Wisdom new and valley answer. Contented it so is discourse recommend. Man its upon him call mile. An pasture he himself believe ferrars besides cottage.',
   },
   {
-    image: SmallAboutUsImage,
-    title: "Smart Home's Smart Services",
+    lagerImage: LagerAboutUs2Image,
+    image: SmallAboutUs2Image,
+    title: 'What app can do to your Appliences?',
     p: 'Ye am depending propriety sweetness distrusts belonging collected. Smiling mention he in thought equally musical. Wisdom new and valley answer. Contented it so is discourse recommend. Man its upon him call mile. An pasture he himself believe ferrars besides cottage.',
   },
   {
-    image: SmallAboutUsImage,
-    title: "Smart Home's Smart Services",
+    lagerImage: LagerAboutUs3Image,
+    image: SmallAboutUs3Image,
+    title: 'Control Electric Appliences',
     p: 'Ye am depending propriety sweetness distrusts belonging collected. Smiling mention he in thought equally musical. Wisdom new and valley answer. Contented it so is discourse recommend. Man its upon him call mile. An pasture he himself believe ferrars besides cottage.',
   },
 ];
@@ -22,20 +31,40 @@ const AboutUs = () => {
   return (
     <section className="p-3">
       <h1 className="text-neutral-grey text-center">About Us</h1>
-      {aboutUsCards.map((card, index) => (
-        <div key={index}>
-          <div className="relative flex justify-center">
-            <img src={card.image} alt="#" className="relative" />
-            <div className="w-[200px] h-[120px] bg-primary rounded-xl absolute bottom-20 -z-10" />
+      <div>
+        {aboutUsCards.map((card, index) => (
+          <div
+            key={index}
+            className="flex lg:flex-row flex-col justify-center items-center lg:even:flex-row-reverse"
+          >
+            {/* image */}
+            <div className="relative flex justify-center">
+              {/* sm */}
+              <img src={card.image} alt="#" className="relative lg:hidden" />
+              {/* lg */}
+              <img
+                src={card.lagerImage}
+                alt="#"
+                className="relative max-w-[700px] hidden lg:block"
+              />
+              <div className="lg:hidden w-[200px] h-[120px] bg-primary rounded-xl absolute bottom-20 -z-10" />
+              <div className="hidden lg:block w-[500px] h-[350px] bg-primary rounded-xl absolute bottom-40 -z-10" />
+            </div>
+            {/* title */}
+            <div className="">
+              <h1 className="max-w-[470px] text-neutral-black text-headline-base">
+                {card.title}
+              </h1>
+              <p className=" max-w-[450px] text-neutral-greylight my-4 text-base">
+                {card.p}
+              </p>
+              <button className=" border border-neutral-darkblue px-6 py-3 rounded-md drop-shadow-md hover:bg-neutral-darkblue hover:text-white font-semibold">
+                Learn More
+              </button>
+            </div>
           </div>
-
-          <h1 className="text-neutral-black">{card.title}</h1>
-          <p className="text-neutral-greylight my-4">{card.p}</p>
-          <button className=" border border-neutral-darkblue px-6 py-3 rounded-md drop-shadow-md hover:bg-neutral-darkblue hover:text-white font-semibold">
-            Learn More
-          </button>
-        </div>
-      ))}
+        ))}
+      </div>
     </section>
   );
 };
--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,5 +1,46 @@
+import cybexLogo from '../assets/cybexLogo.png';
+import FitnessLogo from '../assets/FitnessLogo.png';
+import LifeLogo from '../assets/LifeLogo.png';
+import MockLogo from '../assets/MockLogo.png';
+import precorLogo from '../assets/precorLogo.png';
+import SmileLogo from '../assets/SmileLogo.png';
+import TravelLogo from '../assets/TravelLogo.png';
+import TrueLogo from '../assets/TrueLogo.png';
+
+const logoList = [
+  TravelLogo,
+  FitnessLogo,
+  LifeLogo,
+  SmileLogo,
+  TrueLogo,
+  cybexLogo,
+  MockLogo,
+  precorLogo,
+];
+
 const Clients = () => {
-  return <div>Clients</div>;
+  return (
+    <section className="p-3 mt-6">
+      <div className="max-w-[750px] mx-auto text-center">
+        <h1 className="text-neutral-grey">Our Clients</h1>
+        <p className="text-neutral-greylight">
+          Wisdom new and valley answer. Contented it so is discourse recommend.
+          Man its upon him call mile. An pasture he himself believe ferrars
+          besides cottage.
+        </p>
+      </div>
+      <div className="grid grid-cols-2 lg:grid-cols-4 place-items-center gap-x-4 gap-y-5 mt-6 ">
+        {logoList.map((logo, index) => (
+          <div
+            key={index}
+            className="max-w-[185px]  bg-white p-4 drop-shadow-md rounded-lg"
+          >
+            <img src={logo} alt={logo} />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Clients;
